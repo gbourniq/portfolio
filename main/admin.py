f6669930327abd88f126ecbb2626616b5341a2bc
Guillaume Bournique <guillaume.bournique@eigentech.com>
2019-12-24 03:57:44 +0100

    black test

--- main/admin.py
+++ main/admin.py
@@ -6,28 +6,36 @@ from django.db import models
 
 class ArticleAdmin(admin.ModelAdmin):
     fieldsets = [
-        ("Title/date", {"fields":["article_name", "date_published"]}),
-        ("URL", {"fields":["article_slug"]}),
-        ("Parent Element", {"fields":["subcategory_name"]}),
-        ("Content", {"fields":["summary", "content", "image"]})
+        ("Title/date", {"fields": ["article_name", "date_published"]}),
+        ("URL", {"fields": ["article_slug"]}),
+        ("Parent Element", {"fields": ["subcategory_name"]}),
+        ("Content", {"fields": ["summary", "content", "image"]}),
     ]
     # Overwrite properties for TextFields only
     formfield_overrides = {
-    	models.TextField: {'widget':TinyMCE()},
+        models.TextField: {"widget": TinyMCE()},
     }
+
 
 class SubCategoryAdmin(admin.ModelAdmin):
     fieldsets = [
-        ("Title", {"fields":["subcategory_name", "subcategory_slug"]}),
-        ("Parent Element", {"fields":["category_name"]}),
-        ("Content", {"fields":["summary", "content", "image"]})
+        ("Title", {"fields": ["subcategory_name", "subcategory_slug"]}),
+        ("Parent Element", {"fields": ["category_name"]}),
+        ("Content", {"fields": ["summary", "content", "image"]}),
     ]
     # Overwrite properties for TextFields only
     formfield_overrides = {
-    	models.TextField: {'widget':TinyMCE()},
+        models.TextField: {"widget": TinyMCE()},
     }
-    
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Category details", {"fields": ["category_name", "summary", "image", "category_slug"]})
+    ]
+
+
 # Register your models here.
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(SubCategory, SubCategoryAdmin)
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
--- main/models.py
+++ main/models.py
@@ -1,52 +1,52 @@
 from django.db import models
 from django.utils import timezone
 
+UPLOADS_FOLDER_PATH = "images/"
 
 # Create your models here.\
 class Category(models.Model):
     category_name = models.CharField(max_length=200)
     summary = models.TextField()
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to=UPLOADS_FOLDER_PATH)
     category_slug = models.CharField(max_length=200, default=1)
-    
+
     class Meta:
         verbose_name_plural = "Categories"
-    
+
     def __str__(self):
         return self.category_name
-    
+
+
 class SubCategory(models.Model):
     subcategory_name = models.CharField(max_length=200)
     summary = models.CharField(max_length=200)
     content = models.TextField()
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to=UPLOADS_FOLDER_PATH)
     subcategory_slug = models.CharField(max_length=200, default=1)
-    category_name = models.ForeignKey(Category,
-                                        default=1,
-                                        verbose_name="Category",
-                                        on_delete='SET_DEFAULT')
-    
+    category_name = models.ForeignKey(
+        Category, default=1, verbose_name="Category", on_delete="SET_DEFAULT"
+    )
+
     class Meta:
         verbose_name_plural = "Sub Categories"
-    
+
     def __str__(self):
         return self.subcategory_name
-    
-    
+
+
 class Article(models.Model):
     article_name = models.CharField(max_length=200)
     summary = models.CharField(max_length=200)
     content = models.TextField()
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to=UPLOADS_FOLDER_PATH)
     date_published = models.DateTimeField("date published", default=timezone.now)
     article_slug = models.CharField(max_length=200, default=1)
-    subcategory_name = models.ForeignKey(SubCategory,
-                                    default=1,
-                                    verbose_name="SubCategory",
-                                    on_delete='SET_DEFAULT')
-    
+    subcategory_name = models.ForeignKey(
+        SubCategory, default=1, verbose_name="SubCategory", on_delete="SET_DEFAULT"
+    )
+
     class Meta:
         verbose_name_plural = "Articles"
-    
+
     def __str__(self):
         return self.article_name
