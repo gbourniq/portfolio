af5c07336eb8d26de1de5d29080ba650f68f7811
Guillaume Bournique <guillaume.bournique@eigentech.com>
2020-01-02 00:30:56 +0100

    restructure ansible scripts to work with env variables

--- app/myportfolio/settings.py
+++ app/myportfolio/settings.py
@@ -209,10 +209,11 @@ STATICFILES_DIRS = (
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "gbournique@gmail.com"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
 EMAIL_TIMEOUT = 10
+
 
 # Configuration which writes all logging from the django logger to a local file
 LOGGING = {
