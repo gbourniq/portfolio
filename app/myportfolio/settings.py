21e665dc14c5d5ed6c3957efe01e32cbadab66c2
Guillaume Bournique <guillaume.bournique@eigentech.com>
2020-01-01 23:57:01 +0100

    modified email settings

--- app/myportfolio/settings.py
+++ app/myportfolio/settings.py
@@ -210,7 +210,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "gbournique@gmail.com"
-EMAIL_HOST_PASSWORD = "B2z43za1ttopa!"
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
 EMAIL_TIMEOUT = 10
 
