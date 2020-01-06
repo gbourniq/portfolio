16c8917f5f00c920e41d87a22de4ec79ab91cd17
Guillaume Bournique <guillaume.bournique@eigentech.com>
2020-01-06 16:43:12 +0100

    debug to False

--- app/myportfolio/settings.py
+++ app/myportfolio/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.getenv("DEBUG")
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [os.getenv("ALLOWED_HOSTS")]
 
