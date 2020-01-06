dcc5ae94c938b391fb4ba559e5da12c3a2f852ac
Guillaume Bournique <guillaume.bournique@eigentech.com>
2020-01-06 16:42:33 +0100

    fixed frontend bug added space between cat cards

--- app/myportfolio/settings.py
+++ app/myportfolio/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.getenv("DEBUG")
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [os.getenv("ALLOWED_HOSTS")]
 
