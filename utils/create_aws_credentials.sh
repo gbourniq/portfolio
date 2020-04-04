ffa4fa75f080aeb99fce9b4ed8aaae9bc3687b40
Guillaume Bournique <gbournique@gmail.com>
2020-04-04 19:19:04 +0200

    add util script

--- utils/create_aws_credentials.sh
+++ utils/create_aws_credentials.sh
@@ -4,6 +4,13 @@ mkdir -p ~/.aws
 
 cat > ~/.aws/credentials << EOL
 [default]
-aws_access_key_id = ${AWS_ACCESS_KEY_ID}
-aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
+aws_access_key_id = AKIAVIFROS5RZ4VHRCYS
+aws_secret_access_key = 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
 EOL
+
+cat > ~/.aws/config << EOL
+[profile default]
+region = eu-west-2
+output = json
+EOL
+
