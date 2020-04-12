11733c98d332c9030de8181da62041d9777af2df
Guillaume Bournique <gbournique@gmail.com>
2020-04-12 20:31:29 +0200

    add dev/prod start_webserver scripts, dev include creation of dummy superuser

---
+++ deployment/docker-deployment/app/dev.startup_server.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+cd /home/portfolio/app
+
+echo "Collecting static files"
+python manage.py collectstatic --no-input -v 0
+
+echo "Running migrations"
+python manage.py makemigrations main
+python manage.py migrate
+
+echo "[DEV BUILD] Creating default Django superuser admin/admin."
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'gbournique@gmail.com', 'admin')" | python manage.py shell || true
+
+echo "Starting webserver"
+python manage.py runserver 0.0.0.0:8080
+
---
+++ deployment/docker-deployment/app/prod.startup_server.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cd /home/portfolio/app
+
+echo "Collecting static files"
+python manage.py collectstatic --no-input -v 0
+
+echo "Running migrations"
+python manage.py makemigrations main
+python manage.py migrate
+
+echo "Django superuser must be create manually with python manage.py createsuperuser"
+
+echo "Starting webserver"
+gunicorn portfolio.wsgi:application --bind 0.0.0.0:8080
