02cff593e314f527a2842bc70801e6bd7b37c669
Guillaume Bournique <gbournique@gmail.com>
2020-04-12 20:47:28 +0200

    edited pg backup upload script to use tar instead of zip + initial pg load role

--- scripts/postgres_backup.sh
+++ scripts/postgres_backup.sh
@@ -11,11 +11,12 @@ fi
 service_name=$1
 database_name=$2
 s3_location_base=$3
+db_user=postgres
 
 # Define backup file names
-postgres_backup="${database_name}.sql"
-postgres_backup_dated_zipped="${database_name}_$(date +%Y-%m-%d"_"%H_%M_%S).sql.gz"
-postgres_backup_latest_zipped="${database_name}_latest.sql.gz"
+postgres_backup="${database_name}.tar"
+postgres_backup_dated="${database_name}_$(date +%Y-%m-%d"_"%H_%M_%S).tar"
+postgres_backup_latest="${database_name}_latest.tar"
 
 # Define S3 locations
 s3_postgres_latest=${s3_location_base}/latest/
@@ -30,7 +31,7 @@ if [[ -z $container_id ]]; then
 fi
 
 # Create pg backup file inside the container"
-docker exec ${container_id} pg_dump -U postgres -f /tmp/${postgres_backup} ${database_name}
+docker exec ${container_id} /bin/sh -c "pg_dump -U ${db_user} -F t ${database_name} > /tmp/${postgres_backup}"
 
 # copy file inside container to host
 docker cp ${container_id}:/tmp/${postgres_backup} .
@@ -38,18 +39,15 @@ docker cp ${container_id}:/tmp/${postgres_backup} .
 # remove file in container
 docker exec ${container_id} rm /tmp/${postgres_backup}
 
-# compress
-gzip ${postgres_backup}
-
 # create two copies of backup on host
-cp ${postgres_backup}.gz ${postgres_backup_latest_zipped}
-cp ${postgres_backup}.gz ${postgres_backup_dated_zipped}
+cp ${postgres_backup} ${postgres_backup_latest}
+cp ${postgres_backup} ${postgres_backup_dated}
 
 # upload copies to s3
-aws s3 cp ${postgres_backup_latest_zipped} ${s3_postgres_latest}
-aws s3 cp ${postgres_backup_dated_zipped} ${s3_postgres_all}
+aws s3 cp ${postgres_backup_latest} ${s3_postgres_latest}
+aws s3 cp ${postgres_backup_dated} ${s3_postgres_all}
 
 # cleanup
-rm ${postgres_backup}.gz
-rm ${postgres_backup_latest_zipped}
-rm ${postgres_backup_dated_zipped}
+rm ${postgres_backup}
+rm ${postgres_backup_latest}
+rm ${postgres_backup_dated}
