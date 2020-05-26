b7cc42e37cafa4df3f61a73afedcc1668e85adab
Guillaume Bournique <gbournique@gmail.com>
2020-05-26 23:38:15 +0200

    update run_pytest script to build test image

--- build_steps/ci_pipeline/3_run_pytest.sh
+++ build_steps/ci_pipeline/3_run_pytest.sh
@@ -12,6 +12,18 @@ function exit_error() {
   exit 1
 }
 
+function build_test_image() {
+  docker build -f deployment/docker-build/tests.Dockerfile -t ${IMAGE_REPOSITORY}:tests .
+  SUCCESS "${IMAGE_REPOSITORY}:tests built successfully"
+}
+
+function run_tests() {
+  if ! (docker exec -it app-tests sh -c "cd app && pytest --cov")
+  then
+      exit_error "Some tests have failed! Aborting."
+  fi
+}
+
 function remove_services() {
   INFO "[BUILD=${BUILD}] Removing docker-compose services..."
   docker-compose ${COMPOSE_ARGS} down --remove-orphans || true
@@ -29,13 +41,11 @@ COMPOSE_FILE=deployment/docker-deployment/tests.docker-compose.yml
 COMPOSE_ARGS="-p ${PROJECT_NAME} -f ${COMPOSE_FILE}"
 
 ### Start script
+build_test_image
 remove_services
 start_services
 source ./scripts/check_services_health.sh
-if ! (docker exec -it app-tests sh -c "cd app && pytest --cov")
-then
-    exit_error "Some tests have failed! Aborting."
-fi
+run_tests
 remove_services
 SUCCESS "Run tests successfully with pytest-django" 
 
