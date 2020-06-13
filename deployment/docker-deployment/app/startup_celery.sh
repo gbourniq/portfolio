b2ea2dbe652719a1e5a787840f874419117565fb
Guillaume Bournique <gbournique@gmail.com>
2020-06-14 01:42:26 +0200

    increase mem limit of celery worker pod

--- deployment/docker-deployment/app/startup_celery.sh
+++ deployment/docker-deployment/app/startup_celery.sh
@@ -5,4 +5,5 @@ cd /home/portfoliouser/portfolio/app
 echo "Setting DJANGO_SETTINGS_MODULE to portfolio.settings.docker_settings"
 export DJANGO_SETTINGS_MODULE=portfolio.settings.docker_settings
 
+echo "Starting celery worker"
 celery --app=portfolio.celeryconf worker --loglevel=info --concurrency=10 -n worker1.%h
