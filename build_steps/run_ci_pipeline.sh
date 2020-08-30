ee24ab1bcc0daecebc718d9a7d1f711e17f4428c
Guillaume Bournique <guillaume.bournique@eigentech.com>
2020-08-30 17:45:01 +0100

    ansible scripts debugging

--- build_steps/run_ci_pipeline.sh
+++ build_steps/run_ci_pipeline.sh
@@ -5,14 +5,14 @@ set -e
 # Source environment variable for dev build
 source ./.dev.env
 
-# # Validate secret environment variables are set
-# if [[ $VALIDATION_FAILED == True ]]; then
-#     exit 1
-# fi
+# Validate secret environment variables are set
+if [[ $VALIDATION_FAILED == True ]]; then
+    exit 1
+fi
 
-# CI_PIPELINE_DIR="build_steps/ci_pipeline"
+CI_PIPELINE_DIR="build_steps/ci_pipeline"
 
-# for entry in "$CI_PIPELINE_DIR"/*
-# do
-#   source "$entry"
-# done
+for entry in "$CI_PIPELINE_DIR"/*
+do
+  source "$entry"
+done
